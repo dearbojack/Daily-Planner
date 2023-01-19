694e35c4d81cf1dfe67524463b1d10b203491e73
Dali Han <ross4han@gmail.com>
2023-01-19 10:58:44 +0000

    add timeblock

--- script.js
+++ script.js
@@ -5,8 +5,9 @@
 const today = moment().format("dddd, MMMM Do");
 $('#currentDay').text(today); 
 
-const punchIn = 9;
-const punchOut = 17;
+const punchIn = 0;
+const punchOut = 24;
+var currentHour = moment().hour();
 
 function createArr(min, max) {
     let array = [];
@@ -19,10 +20,33 @@ function createArr(min, max) {
 var worktime = createArr(punchIn, punchOut);
 
 for (let i = 0; i < worktime.length; i++) {
+    // hour row
     var $rowDiv = $("<div class='row'></div>");
+    // hour cell
     var $hourDiv = $("<div class='hour'></div>");
+    // hour text
     var hourText = moment().hour(worktime[i]).format('hh A');
-    $hourDiv.text = hourText
-    $rowDiv.appendChild($hourDiv);
+    // set hour text of hour cell
+    $hourDiv.text(hourText);
+    // time block with text are in it
+    var $timeblock = $("<div class='time-block'></div>");
+    // save button
+    var $saveBtn = $("<button class='saveBtn'></button>")
+
+    // change color of timeblock based on current day
+    if (currentHour < worktime[i]) {
+        // the past
+        $timeblock.addClass('past');
+    } else if (currentHour = worktime[i]) {
+        // the present
+        $timeblock.addClass('present');
+    } else {
+        // the future
+        $timeblock.addClass('future');
+    };
+
+
+    // append to hour row
+    $rowDiv.append($hourDiv, $timeblock, $saveBtn);
     $('.container').append($rowDiv);
 };
