c11b74a0b9fdf0bfe948a5b9259ce937e6cae2ba
Dali Han <ross4han@gmail.com>
2023-01-19 10:37:51 +0000

    append div for each hour row

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-import moment from 'moment';
+// const moment = require('moment');
+// import moment from 'moment.js';
 
 // show date of today in the jumbotron
 const today = moment().format("dddd, MMMM Do");
-$('#currentDay').append(today); 
+$('#currentDay').text(today); 
+
+const punchIn = 9;
+const punchOut = 17;
+
+function createArr(min, max) {
+    let array = [];
+    for (let i = min; i <= max; i++) {
+        array.push(i);
+    }
+    return array;
+};
+
+var worktime = createArr(punchIn, punchOut);
+
+for (let i = 0; i < worktime.length; i++) {
+    var $rowDiv = $("<div class='row'></div>");
+    var $hourDiv = $("<div class='hour'></div>");
+    var hourText = moment().hour(worktime[i]).format('hh A');
+    $hourDiv.text = hourText
+    $rowDiv.appendChild($hourDiv);
+    $('.container').append($rowDiv);
+};
